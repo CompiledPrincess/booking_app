857cd7815fd9eed364b48a71b7a90c1ffbcf6d56
Qing <101427135+CompiledPrincess@users.noreply.github.com>
2023-06-14 16:01:58 +0000

    setup

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to Compiled Princess booking app!")
+	var conferenceName = "Compiled Princess Conference"
+	const conferenceTickets = 333
+	var remainingTickets = 333
+
+	fmt.Printf("Welcome to %v booking app!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets, and %v tickets are availible!\n", conferenceTickets, remainingTickets)
 	fmt.Println("Grab your tickets here!")
+
 }
