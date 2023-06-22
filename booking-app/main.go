0d91188014af0c8215db1e8a23c68f818835b809
Qing <101427135+CompiledPrincess@users.noreply.github.com>
2023-06-22 16:14:43 +0000

    something is wrong

--- booking-app/main.go
+++ booking-app/main.go
@@ -20,33 +20,44 @@ func main() {
 		var email string
 		var bookedTickets uint
 		var bookings = []string{}
+		for remainingTickets > 0 {
+			fmt.Println("Enter your first name")
+			fmt.Scan(&firstName)
 
-		fmt.Println("Enter your first name")
-		fmt.Scan(&firstName)
+			fmt.Println("Enter your last name")
+			fmt.Scan(&lastName)
 
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName)
+			fmt.Println("Enter your email")
+			fmt.Scan(&email)
 
-		fmt.Println("Enter your email")
-		fmt.Scan(&email)
+			fmt.Println("How many tickets are you booking?")
+			fmt.Scan(&bookedTickets)
 
-		fmt.Println("How many tickets are you booking?")
-		fmt.Scan(&bookedTickets)
+			remainingTickets = conferenceTickets - int(bookedTickets)
+			bookings = append(bookings, firstName+" "+lastName)
 
-		remainingTickets = remainingTickets - int(bookedTickets)
-		bookings = append(bookings, firstName+" "+lastName)
+			fmt.Printf("Thank you %v %v,you booked %v tickets. You will receive a confirmation email at %v \n",
+				firstName, lastName, bookedTickets, email)
 
-		fmt.Printf("Thank you %v %v,you booked %v tickets. You will receive a confirmation email at %v \n",
-			firstName, lastName, bookedTickets, email)
+			fmt.Printf("The remaining tickets' total is %v \n", remainingTickets)
 
-		fmt.Printf("The remaining tickets' total is %v \n", remainingTickets)
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of the bookings are: %v\n", firstNames)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			if bookedTickets > uint(remainingTickets) {
+				fmt.Printf("We have %v tickets left.", remainingTickets)
+				continue
+			}
+
+			if remainingTickets == 0 {
+				fmt.Println("The tickets are sold out!")
+				break
+			}
 		}
-		fmt.Printf("The first names of the bookings are %v\n", firstNames)
 	}
 
 }
